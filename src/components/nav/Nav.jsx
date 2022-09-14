3e3132b0f17099aacb1b98c1d4b6c6b8341b1452
Arnav Gupta <arnavgpta@gmail.com>
2022-09-14 20:48:50 +0530

    nav before useState

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {BiUser} from 'react-icons/bi'
+import {MdWorkOutline} from 'react-icons/md'
+import {AiOutlineFundProjectionScreen} from 'react-icons/ai'
+import {BiMessageSquareDetail} from 'react-icons/bi'
+import { useState } from 'react'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('');
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#" className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
+      <a href="#about"><BiUser/></a>
+      <a href="#experience"><MdWorkOutline/></a>
+      <a href="#portfolio"><AiOutlineFundProjectionScreen/></a>
+      <a href="#contact"><BiMessageSquareDetail /></a>
+    </nav>
   )
 }
 
